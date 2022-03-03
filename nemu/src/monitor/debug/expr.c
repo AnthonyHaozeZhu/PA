710b88711bc10d84b59fed71af49c5719ea581c4
tracer-ics2017 <tracer@njuics.org>
2022-03-03 20:37:56 +0800

    >  compile 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:37:56 up 14 days, 12:32,  0 users,  load average: 0.01, 0.05, 0.02 2013bd325732eada67a11b511044ce7fdd8f5f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -277,8 +277,9 @@ uint32_t eval(int p, int q) {
       case TK_NEGATIVE: //负号
         return -eval(p + 1, q);
       case TK_DEREF: //指针求值
-        printf("haahahhahah");
+        printf("1\n");
         addr = eval(p + 1, q);
+        printf("2\n");
         result = vaddr_read(addr, 4);
         printf("adddr=%u(0x%x)---->value=%d(0x%08x\n", addr, addr, result, result);
         return result;
