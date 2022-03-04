05e28654664b9469dc86bcba24879c75ba8d9a87
AnthonyHaozeZhu <1119886232@qq.com>
2022-03-04 16:06:59 +0800

    完成打印监视点辅助函数，但未进行测试

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -8,8 +8,15 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-
+  int old; //旧的值
+  char e[32]; //表达式
+  int hitNum; //记录触发次数
 
 } WP;
+
+bool new_wp(char*);
+bool free_wp(int);
+void print_wp();
+bool watch_wp();
 
 #endif
