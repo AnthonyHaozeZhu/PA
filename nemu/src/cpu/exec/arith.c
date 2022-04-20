d03d41140eb61ddee7fcd8808237d2e795acad52
tracer-ics2017 <tracer@njuics.org>
2022-04-20 13:45:31 +0800

    >  compile 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:45:31 up 55 min,  3 users,  load average: 0.20, 0.07, 0.05 212cf76f6e7112e22fd15c8280aeeb5bdad63a0f

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -54,7 +54,7 @@ make_EHelper(inc) {
   rtl_addi(&t2, &id_dest->val, 1);
   operand_write(id_dest, &t2);
   rtl_update_ZFSF(&t2, id_dest->width);
-  rtl_eqi(&t0,&t2,0x80000000);
+  rtl_eqi(&t0, &t2, 0x80000000);
   rtl_set_OF(&t0);
   print_asm_template1(inc);
 }
@@ -65,7 +65,7 @@ make_EHelper(dec) {
   rtl_subi(&t2, &id_dest->val, 1);
   operand_write(id_dest, &t2);
   rtl_update_ZFSF(&t2, id_dest->width);
-  rtl_eqi(&t0,&t2,0x7fffffff);
+  rtl_eqi(&t0, &t2, 0x7fffffff);
   rtl_set_OF(&t0);
   print_asm_template1(dec);
 }
