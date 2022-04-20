a548fc6c35b28fa49524cd2bc7d73d314a2d8b61
tracer-ics2017 <tracer@njuics.org>
2022-04-20 16:25:49 +0800

    >  compile 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:25:49 up  3:35,  3 users,  load average: 3.03, 1.93, 2.51 5e86af34d67b277784c8d324a04b66e861c81319

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -92,3 +92,12 @@ make_EHelper(not) {
   operand_write(id_dest,&id_dest->val);
   print_asm_template1(not);
 }
+
+make_EHelper(rol) {
+	rtl_shri(&t2, &id_dest->val, id_dest->width * 8 - id_src->val);
+	rtl_shl(&t3, &id_dest->val, &id_src->val);
+	rtl_or(&t1, &t2, &t3);
+	operand_write(id_dest, &t1);
+
+  print_asm_template2(rol);
+}
--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -23,9 +23,15 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = i;
+  // int i;
+  // for (i = 0; i < _screen.width * _screen.height; i++) {
+  //   fb[i] = i;
+  // }
+  int temp = (w > _screen.width - x)?_screen.width -x :w;
+  int cp_bytes = sizeof(uint32_t) *temp;
+  for(int j = 0; j < h && y + j < _screen.height; j++) {
+    memcpy(&fb[(y + j) * _screen.width + x], pixels, cp_bytes);
+    pixels += w;
   }
 }
 
