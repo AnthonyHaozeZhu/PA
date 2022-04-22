18bfa759de36ebccf846d75abc63570775a7bb7e
tracer-ics2017 <tracer@njuics.org>
2022-04-22 20:47:21 +0800

    >  compile 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:47:21 up  3:17,  1 user,  load average: 0.10, 0.24, 0.25 d6dcbcd95740dbb2662cc9d519e76e3be875c9e

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -56,12 +56,15 @@ make_EHelper(cltd) {
 
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
-    TODO();
-  }
-  else {
-    TODO();
-  }
-
+      rtl_lr_b(&t0, R_AX);
+      rtl_sext(&t0, &t0, 1);
+      rtl_sr_w(R_AX, &t0);
+    } 
+    else {
+      rtl_lr_w(&t0, R_AX);
+      rtl_sext(&t0, &t0, 2);
+      rtl_sr_l(R_EAX, &t0);
+    }
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
 }
 
