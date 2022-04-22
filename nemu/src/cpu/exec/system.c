ea4fef2353910362af2a6e44e3dfa820270674e4
tracer-ics2017 <tracer@njuics.org>
2022-04-22 16:10:07 +0800

    >  compile 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:10:07 up  6:13,  1 user,  load average: 0.90, 1.40, 0.78 87203b65fd4996c6795135f3acea00e53d481b4e

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -56,7 +56,7 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  pio_write(id_dest->val, id_src->val, id_src->width);
+  pio_write(id_dest->val, id_src->width, id_src->val);
 
   print_asm_template2(out);
 
