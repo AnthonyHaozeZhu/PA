46367916e31bdba9f56cbec07455c3ecb789c431
tracer-ics2017 <tracer@njuics.org>
2022-05-14 01:49:02 +0800

    >  run 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:49:02 up 2 days,  1:02,  0 users,  load average: 0.07, 0.06, 0.01 407e7fe447de0acee3c86ff6f4b186b8a4208c1e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,6 +9,21 @@ void sys_exit(int a){
   _halt(a);
 }
 
+int sys_write(int fd, void *buf, size_t len) {
+	if(fd == 1 || fd == 2){
+		char c;
+		for(int i = 0; i < len; i++) {
+			memcpy(&c ,buf + i, 1);
+			_putc(c);
+		}
+		return len;
+	}
+  else{
+    panic("Unhandled fd=%d in sys_write()",fd);
+  }
+	return -1;			
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -22,6 +37,9 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_exit: 
       sys_exit(a[1]);
+      break;
+    case SYS_write:
+      SYSCALL_ARG1(r) = sys_write(a[1], (void*)a[2], a[3]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
