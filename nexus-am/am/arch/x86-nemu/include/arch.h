47107ef7ae9158eaab38d2f60005333339efaf09
tracer-ics2017 <tracer@njuics.org>
2022-05-14 01:24:28 +0800

    >  compile 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:24:28 up 2 days, 38 min,  0 users,  load average: 0.08, 0.02, 0.01 9c5d07745d9e6e5d77d807c0f9477f849dc9dc6d

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -7,8 +7,13 @@
 #define PGSIZE    4096    // Bytes mapped by a page
 
 struct _RegSet {
-  uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
+  // uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
+  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
   int       irq;
+  uintptr_t error_code;
+  uintptr_t eip;
+  uintptr_t cs;
+  uintptr_t eflags;
 };
 
 #define SYSCALL_ARG1(r) 0
