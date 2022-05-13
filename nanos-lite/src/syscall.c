35b537441324e586ea3fe63812ab7b5a8b2349e5
tracer-ics2017 <tracer@njuics.org>
2022-05-14 01:34:42 +0800

    >  run 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:34:42 up 2 days, 48 min,  0 users,  load average: 0.01, 0.07, 0.02 cb7afbc783cf09e79f326570a57b35cdc032aa81

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,31 @@
 #include "common.h"
 #include "syscall.h"
 
+int sys_none() {
+  return 1;
+}
+
+void sys_exit(int a){
+  _halt(a);
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
+    case SYS_none: 
+      SYSCALL_ARG1(r) = sys_none();
+      break;
+    case SYS_exit: 
+      sys_exit(a[1]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
 }
+
--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -16,10 +16,11 @@ struct _RegSet {
   uintptr_t eflags;
 };
 
-#define SYSCALL_ARG1(r) 0
-#define SYSCALL_ARG2(r) 0
-#define SYSCALL_ARG3(r) 0
-#define SYSCALL_ARG4(r) 0
+#define SYSCALL_ARG1(r) r -> eax
+#define SYSCALL_ARG2(r) r -> ebx
+#define SYSCALL_ARG3(r) r -> ecx
+#define SYSCALL_ARG4(r) r -> edx
+
 
 #ifdef __cplusplus
 extern "C" {
