6e8a7d52b112b201af5ce2d5350db05dcad5f90b
tracer-ics2017 <tracer@njuics.org>
2022-05-14 20:38:22 +0800

    >  run 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:38:22 up  4:44,  1 user,  load average: 4.97, 2.09, 1.02 8a45cf4bb18aadb653179c1c4475a126f28e2836

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -3,6 +3,9 @@
 #define NAME(key) \
   [_KEY_##key] = #key,
 
+
+extern void getScreen(int* p_width, int* p_height);
+
 static const char *keyname[256] __attribute__((used)) = {
   [_KEY_NONE] = "NONE",
   _KEYS(NAME)
@@ -18,35 +21,38 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
   strncpy(buf, dispinfo + offset, len);
 }
 
-extern void getScreen(int *p_width, int *p_height);
 void fb_write(const void *buf, off_t offset, size_t len) {
-  assert(offset % 4 == 0 && len % 4 != 0);
+  assert(offset % 4 == 0 && len % 4 == 0);
   int index, screen_x1, screen_y1, screen_y2;
-  int width = 0; 
-  int height = 0;
+  int width=0,height=0;
   getScreen(&width, &height);
-  index = offset / 4;
-  screen_y1 = index / width;
-  screen_x1 = index % width;
-  index  = (offset + len) / 4;
-  screen_y2 = index / width;
-  assert(screen_y2 >= screen_y1);
+  index=offset/4;
+  screen_y1=index/width;
+  screen_x1=index%width;
 
-  if(screen_y2 == screen_y1) {
-    _draw_rect(buf, screen_x1, screen_y1, len / 4, 1);
-    return;
+  index=(offset+len)/4;
+  screen_y2=index/width;
+
+  assert(screen_y2>=screen_y1);
+
+  if(screen_y2==screen_y1)
+  {
+    _draw_rect(buf,screen_x1,screen_y1,len/4,1);
+    return ;
   }
 
-  int tempw = width - screen_x1;
-  if(screen_y2 - screen_y1 == 1) {
-    _draw_rect(buf, screen_x1, screen_y1, tempw, 1);
-    _draw_rect(buf + tempw * 4, 0, screen_y2, len / 4 - tempw, 1);
-    return;
+  int tempw=width-screen_x1;
+  if(screen_y2-screen_y1==1)
+  {
+    _draw_rect(buf,screen_x1,screen_y1,tempw,1);
+    _draw_rect(buf+tempw * 4 ,0,screen_y2,len/4-tempw,1);
+    return ;
   }
   _draw_rect(buf, screen_x1, screen_y1, tempw, 1);
   int tempy = screen_y2 - screen_y1 - 1;
   _draw_rect(buf + tempw * 4, 0, screen_y1 + 1, width, tempy);
-  _draw_rect(buf + tempw * 4 + tempy * width * 4, 0, screen_y2, len / 4 - tempw - tempy * width, 1);
+  _draw_rect(buf+tempw*4+tempy*width*4,0,screen_y2, len / 4 - tempw - tempy * width, 1);
+
 }
 
 void init_device() {
@@ -54,8 +60,7 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
-  int w = 0;
-  int h = 0;
-  getScreen(&w, &h);
-  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", w, h);
+  int width = 0, height = 0;
+  getScreen(&width, &height);
+  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", width,height);
 }
