5e17a635ebebffeaa01213cfe40143b544aa5390
tracer-ics2017 <tracer@njuics.org>
2022-05-14 12:42:45 +0800

    >  run 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:42:45 up 2 days, 11:56,  0 users,  load average: 0.43, 0.19, 0.08 1528a7ab9231ce71f2df464073226809c37afa95

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -30,8 +30,24 @@ int sys_write(int fd, void *buf, size_t len) {
 	return -1;			
 }
 
+int sys_open(const char *pathname,int flags,int mode){
+    return fs_open(pathname,flags,mode);
+}
+
+int sys_read(int fd, void *buf,size_t len){
+    return fs_read(fd, buf, len);
+}
+
+int sys_lseek(int fd, off_t offset, int whence) {
+    return fs_lseek(fd,offset, whence);
+}
+
 int sys_brk(int addr) {
   return 0;
+}
+
+int sys_close(int fd){
+    return fs_close(fd);
 }
 
 _RegSet* do_syscall(_RegSet *r) {
@@ -54,6 +70,14 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_brk:
       SYSCALL_ARG1(r) = sys_brk(a[1]);
       break;
+    case SYS_read:
+      SYSCALL_ARG1(r)=sys_read(a[1],(void*)a[2],a[3]);
+      break;
+    case SYS_open:
+      SYSCALL_ARG1(r)=sys_open((char*)a[1],a[2],a[3]);
+      break;
+    case SYS_close:SYSCALL_ARG1(r)=sys_close(a[1]);break;
+    case SYS_lseek:SYSCALL_ARG1(r)=sys_lseek(a[1],a[2],a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
