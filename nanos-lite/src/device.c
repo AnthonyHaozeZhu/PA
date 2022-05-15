2473572d4e0289ad06c7a3918d39ecd594338dce
tracer-ics2017 <tracer@njuics.org>
2022-05-15 16:32:03 +0800

    >  run 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:32:03 up 22 min,  1 user,  load average: 0.12, 0.84, 1.07 2ff76fe9d112b8181b50098be248484edcd7a30e

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,15 +15,15 @@ size_t events_read(void *buf, size_t len) {
   char buffer[40];
   int key = _read_key();
   int down = 0;
-  if(key & 0x8000){
+  if(key & 0x8000) {
       key ^= 0x8000;
       down = 1;
   }
   if(key != _KEY_NONE) {
-     sprintf(buffer, "%s %s\n", down ? "kd": "ku", keyname[key]);
+    //  sprintf(buffer, "%s %s\n", down ? "kd": "ku", keyname[key]);
   }
   else {
-      sprintf(buffer,"t %d\n", _uptime());
+      // sprintf(buffer,"t %d\n", _uptime());
   }
   if(strlen(buffer) <= len) { 
     strncpy((char*)buf, buffer,strlen(buffer));
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,7 +13,7 @@ void sys_exit(int a){
 int sys_write(int fd, void *buf, size_t len) {
 	if(fd == 1 || fd == 2){
 		char c;
-    Log("buffer:%s", (char*)buf);
+    // Log("buffer:%s", (char*)buf);
 		for(int i = 0; i < len; i++) {
 			memcpy(&c ,buf + i, 1);
 			_putc(c);
