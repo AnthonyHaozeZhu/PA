e011816e4c0cf9b12c53fbfba0db08761a6beeb4
tracer-ics2017 <tracer@njuics.org>
2022-06-12 18:09:14 +0800

    >  run 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:09:14 up 28 days, 41 min,  1 user,  load average: 1.44, 2.15, 2.25 fae712192563f18695948e733bce3d6f2569e39

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -6,31 +6,31 @@
 typedef int FLOAT;
 
 static inline int F2int(FLOAT a) {
-  if((a & 0x80000000) ==0) {
-    return a >> 16;
-  }
-  else {
-    return -((-a) >> 16);
-  }
-  // assert(0);
-  // return 0;
+  // if((a & 0x80000000) ==0) {
+  //   return a >> 16;
+  // }
+  // else {
+  //   return -((-a) >> 16);
+  // }
+  assert(0);
+  return 0;
 }
 
 static inline FLOAT int2F(int a) {
-  int b;
-  if(a < 0) {
-    b = -a;
-  }
-  else {
-    b = a;
-  }
-  FLOAT result = (b << 16);
-  if(a < 0) {
-    result = - result;
-  }
-  return result;
-  // assert(0);
-  // return 0;
+  // int b;
+  // if(a < 0) {
+  //   b = -a;
+  // }
+  // else {
+  //   b = a;
+  // }
+  // FLOAT result = (b << 16);
+  // if(a < 0) {
+  //   result = - result;
+  // }
+  // return result;
+  assert(0);
+  return 0;
 }
 
 FLOAT f2F(float);
@@ -41,19 +41,19 @@ FLOAT Fsqrt(FLOAT);
 FLOAT Fpow(FLOAT, FLOAT);
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
-  b = int2F(b);
-  FLOAT result = F_mul_F(a, b);
-  return result;
-  // assert(0);
-  // return 0;
+  // b = int2F(b);
+  // FLOAT result = F_mul_F(a, b);
+  // return result;
+  assert(0);
+  return 0;
 }
 
 static inline FLOAT F_div_int(FLOAT a, int b) {
-  b = int2F(b);
-  FLOAT result = F_div_F(a, b);
-  return result;
-  // assert(0);
-  // return 0;
+  // b = int2F(b);
+  // FLOAT result = F_div_F(a, b);
+  // return result;
+  assert(0);
+  return 0;
 }
 
 #endif
