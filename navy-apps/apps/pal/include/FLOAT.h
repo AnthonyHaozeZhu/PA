0d39ff404864382215f00de4adef64cffd4af04b
tracer-ics2017 <tracer@njuics.org>
2022-06-12 15:10:26 +0800

    >  run 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:10:26 up 27 days, 21:43,  1 user,  load average: 0.25, 0.47, 0.29 c1c6280251b2e9e3a7df8b043d67a48491cf23f

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -6,8 +6,14 @@
 typedef int FLOAT;
 
 static inline int F2int(FLOAT a) {
-  assert(0);
-  return 0;
+  if((a & 0x80000000) ==0) {
+    return a >> 16;
+  }
+  else {
+    return -((-a) >> 16);
+  }
+  // assert(0);
+  // return 0;
 }
 
 static inline FLOAT int2F(int a) {
