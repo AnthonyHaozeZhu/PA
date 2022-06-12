65dcfe199808e109d05f01f29d684cdadbb6f7a2
tracer-ics2017 <tracer@njuics.org>
2022-06-12 18:02:25 +0800

    >  run 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:02:25 up 28 days, 35 min,  1 user,  load average: 1.12, 1.99, 2.23 bc655bf28785c978583220e9ef086ef2765fecb

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -3,34 +3,34 @@
 #include <assert.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  long long int result =  (long long int) a * b;
-  int *temp = (int *)& result;
-  return (FLOAT)((*(temp)>>16)& 0xffff) | (*(temp + 1) << 16);
+  // long long int result =  (long long int) a * b;
+  // int *temp = (int *)& result;
+  // return (FLOAT)((*(temp)>>16)& 0xffff) | (*(temp + 1) << 16);
   
-  // assert(0);
-  // return 0;
+  assert(0);
+  return 0;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
 
-  FLOAT x = Fabs(a);
-  FLOAT y = Fabs(b);
-  FLOAT result = x / y;
-  x = x % y;
-  for (int i = 0; i < 16; i++) {
-    x <<= 1;
-    result <<= 1;
-    if (x >= y) {
-      x -= y;
-      result++;
-    }
-  }
-  if (((a ^ b) & 0x80000000) == 0x80000000) {
-    result = -result; 
-  }
-  return result;
-  // assert(0);
-  // return 0;
+  // FLOAT x = Fabs(a);
+  // FLOAT y = Fabs(b);
+  // FLOAT result = x / y;
+  // x = x % y;
+  // for (int i = 0; i < 16; i++) {
+  //   x <<= 1;
+  //   result <<= 1;
+  //   if (x >= y) {
+  //     x -= y;
+  //     result++;
+  //   }
+  // }
+  // if (((a ^ b) & 0x80000000) == 0x80000000) {
+  //   result = -result; 
+  // }
+  // return result;
+  assert(0);
+  return 0;
 }
 
 FLOAT f2F(float a) {
@@ -43,38 +43,38 @@ FLOAT f2F(float a) {
    * stack. How do you retrieve it to another variable without
    * performing arithmetic operations on it directly?
    */
-  Log("This is in it!");
-  union float_ {
-    struct {
-      uint32_t m: 23; //浮点数表示的有效位
-      uint32_t e: 8;  //浮点数表示的指数位
-      uint32_t s: 1;  //浮点数表示的符号位
-      /* data */
-    };
-    uint32_t value;
-  };
-  uint32_t temp = 0;
-  memcpy(&temp, &a, sizeof(a));
-  union float_ f;
-  f.value = temp;
-  int e = f.e - 127;
+  // Log("This is in it!")
+  // union float_ {
+  //   struct {
+  //     uint32_t m: 23; //浮点数表示的有效位
+  //     uint32_t e: 8;  //浮点数表示的指数位
+  //     uint32_t s: 1;  //浮点数表示的符号位
+  //     /* data */
+  //   };
+  //   uint32_t value;
+  // };
+  // uint32_t temp = 0;
+  // memcpy(&temp, &a, sizeof(a));
+  // union float_ f;
+  // f.value = temp;
+  // int e = f.e - 127;
 
-  FLOAT result;
+  // FLOAT result;
 
-  if (e <= 7) {
-    result = (f.m | (1 << 23)) >> 7 - e;//1<<23代表缺省的1 
-  }
-  else {
-    result = (f.m | (1 << 23)) << (e - 7);
-  }
-  if(f.s == 0) {
-    return result;
-  }
-  else {
-    return (result | (1<<31));
-  }
-    // assert(0);
-  // return 0;
+  // if (e <= 7) {
+  //   result = (f.m | (1 << 23)) >> 7 - e;//1<<23代表缺省的1 
+  // }
+  // else {
+  //   result = (f.m | (1 << 23)) << (e - 7);
+  // }
+  // if(f.s == 0) {
+  //   return result;
+  // }
+  // else {
+  //   return (result | (1<<31));
+  // }
+    assert(0);
+  return 0;
 }
 
 FLOAT Fabs(FLOAT a) {
