91d496ba9e114e77100e20bc43d6bd53c97ed411
tracer-ics2017 <tracer@njuics.org>
2022-06-12 17:51:00 +0800

    >  run 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:51:00 up 28 days, 23 min,  1 user,  load average: 0.51, 1.74, 2.33 31226ff39a8ffb568eb5af056e272a8260c73f07

---
+++ navy-apps/fsimg/share/games/pal/pal/include/FLOAT.h
@@ -0,0 +1,43 @@
+#ifndef __FLOAT_H__
+#define __FLOAT_H__
+
+#include "assert.h"
+
+typedef int FLOAT;
+
+static inline int F2int(FLOAT a) 
+{
+  
+  if ((a & 0x80000000) == 0)
+    return a >> 16;
+  else
+    return -((-a) >> 16);
+}
+
+static inline FLOAT int2F(int a) 
+{
+  if ((a & 0x80000000) == 0)
+    return a << 16;
+  else
+    return -((-a) << 16);
+}
+
+static inline FLOAT F_mul_int(FLOAT a, int b) 
+{
+  return a*b;
+}
+
+static inline FLOAT F_div_int(FLOAT a, int b) 
+{
+  assert(b != 0);
+  return a/b;
+}
+
+FLOAT f2F(float);
+FLOAT F_mul_F(FLOAT, FLOAT);
+FLOAT F_div_F(FLOAT, FLOAT);
+FLOAT Fabs(FLOAT);
+FLOAT Fsqrt(FLOAT);
+FLOAT Fpow(FLOAT, FLOAT);
+
+#endif
---
+++ navy-apps/fsimg/share/games/pal/pal/src/FLOAT/FLOAT.c
@@ -0,0 +1,107 @@
+#include "FLOAT.h"
+#include <stdint.h>
+#include <assert.h>
+
+FLOAT F_mul_F(FLOAT a, FLOAT b) 
+{
+  return ((int64_t)a * (int64_t)b) >> 16;
+}
+
+FLOAT F_div_F(FLOAT a, FLOAT b) 
+{
+  assert(b != 0);
+  FLOAT x = Fabs(a);
+  FLOAT y = Fabs(b);
+  FLOAT ret = x / y;
+  x = x % y;
+
+  for (int i = 0; i < 16; i++) 
+  {
+    x <<= 1;
+    ret <<= 1;
+    if (x >= y) 
+    {
+      x -= y;
+      ret++;
+    }
+  }
+  if (((a ^ b) & 0x80000000) == 0x80000000) 
+  {
+    ret = -ret;
+  }
+  return ret;
+}
+
+FLOAT f2F(float a) 
+{
+  /* You should figure out how to convert `a' into FLOAT without
+   * introducing x87 floating point instructions. Else you can
+   * not run this code in NEMU before implementing x87 floating
+   * point instructions, which is contrary to our expectation.
+   *
+   * Hint: The bit representation of `a' is already on the
+   * stack. How do you retrieve it to another variable without
+   * performing arithmetic operations on it directly?
+   */
+
+  union float_ 
+  {
+    struct {
+      uint32_t man : 23;
+      uint32_t exp : 8;
+      uint32_t sign : 1;
+    };
+    uint32_t val;
+  };
+  union float_ f;
+  f.val = *((uint32_t*)(void*)&a);
+  int exp = f.exp - 127;
+  FLOAT ret = 0;
+  if (exp == 128)
+    assert(0);
+  if (exp >= 0) 
+  {
+    int mov = 7 - exp;
+    if (mov >= 0)
+      ret = (f.man | (1 << 23)) >> mov;
+    else
+      ret = (f.man | (1 << 23)) << (-mov);
+  }
+  else
+    return 0;
+  return f.sign == 0 ? ret : -ret;
+}
+
+FLOAT Fabs(FLOAT a) 
+{
+  if ((a & 0x80000000) == 0)
+    return a;
+  else
+    return -a;
+}
+
+/* Functions below are already implemented */
+
+FLOAT Fsqrt(FLOAT x) {
+  FLOAT dt, t = int2F(2);
+
+  do {
+    dt = F_div_int((F_div_F(x, t) - t), 2);
+    t += dt;
+  } while(Fabs(dt) > f2F(1e-4));
+
+  return t;
+}
+
+FLOAT Fpow(FLOAT x, FLOAT y) {
+  /* we only compute x^0.333 */
+  FLOAT t2, dt, t = int2F(2);
+
+  do {
+    t2 = F_mul_F(t, t);
+    dt = (F_div_F(x, t2) - t) / 3;
+    t += dt;
+  } while(Fabs(dt) > f2F(1e-4));
+
+  return t;
+}
