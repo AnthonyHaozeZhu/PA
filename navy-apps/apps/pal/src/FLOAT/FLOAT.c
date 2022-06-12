1d55e941d044a2efbd9d497431403ce65f4b5b5e
tracer-ics2017 <tracer@njuics.org>
2022-06-12 16:30:02 +0800

    >  run 1911530 lighthouse Linux VM-24-4-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:30:02 up 27 days, 23:02,  1 user,  load average: 0.09, 0.04, 0.08 4b94b3f87f8f6841f035a141567447a9ed1ac02c

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -3,13 +3,32 @@
 #include <assert.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+  long long int result =  (long long int) a * b;
+  int *temp = (int *)& result;
+  return (FLOAT)((*(temp)>>16)& 0xffff) | (*(temp + 1) << 16);
+  
+  // assert(0);
+  // return 0;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+
+  FLOAT x = Fabs(a);
+  FLOAT y = Fabs(b);
+  FLOAT result = x / y;
+  x = x % y;
+  for (int i = 0; i < 16; i++) {
+    x <<= 1;
+    result <<= 1;
+    if (x >= y) {
+x -= y;
+result++; }
+  }
+  if (((a ^ b) & 0x80000000) == 0x80000000) {
+result = -result; }
+return result;
+  // assert(0);
+  // return 0;
 }
 
 FLOAT f2F(float a) {
@@ -23,8 +42,37 @@ FLOAT f2F(float a) {
    * performing arithmetic operations on it directly?
    */
 
-  assert(0);
-  return 0;
+  union float_ {
+    struct {
+      uint32_t m: 23; //浮点数表示的有效位
+      uint32_t e: 8;  //浮点数表示的指数位
+      uint32_t s: 1;  //浮点数表示的符号位
+      /* data */
+    };
+    uint32_t value;
+  };
+  uint32_t temp = 0;
+  memcpy(&temp, &a, sizeof(a));
+  union float_ f;
+  f.value = temp;
+  int e = f.e - 127;
+
+  FLOAT result;
+
+  if (e <= 7) {
+    result = (f.m | (1 << 23)) >> 7 - e;//1<<23代表缺省的1 
+  }
+  else {
+    result = (f.m | (1 << 23)) << (e - 7);
+  }
+  if(f.s == 0) {
+    return result;
+  }
+  else {
+    return (result | (1<<31));
+  }
+    // assert(0);
+  // return 0;
 }
 
 FLOAT Fabs(FLOAT a) {
